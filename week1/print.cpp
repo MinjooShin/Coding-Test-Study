71181fe32e5e4e86c7e9764659a45c4a15eb2dc0
minjoo <minjooshin@handong.ac.kr>
2024-01-03 05:08:56 +0900

    [PGS] Level0 출력

---
+++ week1/print.cpp
@@ -0,0 +1,21 @@
+/*
+출처: 프로그래머스
+문제: [PCCE 기출문제] 1번 / 출력
+난이도: Level 0
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/250133
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string msg = "Spring is beginning";
+    int val1 = 3;
+    string val2 = "3";
+
+    cout << msg << endl;
+    cout << val1 + 10 << endl;
+    cout << val2 + "10" << endl;
+
+    return 0;
+}
