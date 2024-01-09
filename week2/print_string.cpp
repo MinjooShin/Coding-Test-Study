03632eaf208f389e74660225b0242b218ceb6534
minjoo <minjooshin@handong.ac.kr>
2024-01-09 16:54:23 +0900

    [PGS] Level0 문자열 출력하기

---
+++ week2/print_string.cpp
@@ -0,0 +1,15 @@
+/*
+출처: 프로그래머스
+문제: [PCCE 기출문제] 8번 / 문자열 출력하기
+난이도: Level 0
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/181952
+*/
+#include <stdio.h>
+#define LEN_INPUT 1000001
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    printf("%s", s1);
+    return 0;
+}
