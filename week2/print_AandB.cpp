6e545df876e2a356df24a1c5b3f0e769131f5b06
minjoo <minjooshin@handong.ac.kr>
2024-01-09 16:58:21 +0900

    [PGS] Level0 a와 b 출력하기

---
+++ week2/print_AandB.cpp
@@ -0,0 +1,16 @@
+/*
+출처: 프로그래머스
+문제: [PCCE 기출문제] 8번 / a와 b 출력하기
+난이도: Level 0
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/181951
+*/
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("a = %d\n", a);
+    printf("b = %d", b);
+    return 0;
+}
