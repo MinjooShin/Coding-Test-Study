199d747dd781cfd6641f45c5f7916016dafb1c74
minjoo <minjooshin@handong.ac.kr>
2024-01-10 21:41:50 +0900

    [PGS] Level0 덧셈식 출력하기

---
+++ week2/print_addition_fomula.cpp
@@ -0,0 +1,17 @@
+/*
+출처: 프로그래머스
+문제: 덧셈식 출력하기
+난이도: Level 0
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/181947
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a+b << endl;
+    return 0;
+}
