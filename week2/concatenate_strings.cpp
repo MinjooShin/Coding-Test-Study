608ceb1ff8ab0594729b5ffb69d650cf6a5075ad
minjoo <minjooshin@handong.ac.kr>
2024-01-10 21:48:58 +0900

    [PGS] Level0 문자열 붙여서 출력하기

---
+++ week2/concatenate_strings.cpp
@@ -0,0 +1,17 @@
+/*
+출처: 프로그래머스
+문제: 문자열 붙여서 출력하기
+난이도: Level 0
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2;
+    cin >> str1 >> str2;
+    cout << str1 + str2;
+    return 0;
+}
