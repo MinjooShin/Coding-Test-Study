804bbc08269744fe674d12b8482235e6d3610229
minjoo <minjooshin@handong.ac.kr>
2024-04-09 22:57:10 +0900

    [PGS] Level0 접미사인지 확인하기

--- week9/checkSuffix.cpp
+++ week9/checkSuffix.cpp
@@ -15,3 +15,13 @@ int solution(string my_string, string is_suffix) {
     if ((my_string.find(is_suffix) != string::npos) && (my_string[my_string.length()-1] == is_suffix[is_suffix.length()-1])) return 1;
     else return 0;
 }
+
+
+// another method
+int solution(string my_string, string is_suffix) {    
+    if(my_string.size() < is_suffix.size())
+    {
+        return 0;
+    }
+    return my_string.substr(my_string.size() - is_suffix.size()) == is_suffix;
+}
