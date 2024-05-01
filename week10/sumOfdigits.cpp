367af614a26d6c86f41d820f2b756a3ac904e365
minjoo <minjooshin@handong.ac.kr>
2024-05-02 03:28:28 +0900

    [PGS] Level1 자릿수 더하기

---
+++ week10/sumOfdigits.cpp
@@ -0,0 +1,22 @@
+/*
+출처: 프로그래머스
+문제: 자릿수 더하기
+난이도: Level1
+시간: 2분
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/12931
+*/
+
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    
+    while (n > 0) {
+        answer += (n%10);
+        n /= 10;
+    }
+
+    return answer;
+}
