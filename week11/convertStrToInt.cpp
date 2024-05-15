75ad5289dea8edf4f6ba6e188061266a7de31871
minjoo <minjooshin@handong.ac.kr>
2024-05-15 17:32:49 +0900

    [PGS] Level1 문자열을 정수로 바꾸기

---
+++ week11/convertStrToInt.cpp
@@ -0,0 +1,18 @@
+/*
+출처: 프로그래머스
+문제: 문자열을 정수로 바꾸기
+난이도: Level1
+시간: 3분
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/12925
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    answer = stoi(s);
+    return answer;
+}
