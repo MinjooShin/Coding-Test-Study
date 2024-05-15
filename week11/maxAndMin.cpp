94f4dbef593fbca150f38f880ff24d3765ba947b
minjoo <minjooshin@handong.ac.kr>
2024-05-15 19:04:17 +0900

    [PGS] Level2 최댓값과 최솟값

--- week11/maxAndMin.cpp
+++ week11/maxAndMin.cpp
@@ -37,3 +37,28 @@ string solution(string s) {
     
     return answer;
 }
+
+// update code
+string solution(string s) {
+    vector <int> v; 
+    string answer = "";
+    string digit = "";
+    s += ' ';
+    for (int i=0; i<=s.length(); i++) {
+        if (s[i] != ' ') {
+            digit += s[i];
+        }
+        else {
+            v.push_back(stoi(digit));
+            digit = "";
+        }
+    }
+    
+    int max = *max_element(v.begin(), v.end());
+    int min = *min_element(v.begin(), v.end());
+    answer += to_string(min);
+    answer += " ";
+    answer += to_string(max);
+    
+    return answer;
+}
