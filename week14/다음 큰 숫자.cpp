4b3f2f6499027c195f55473405c63b5294b09c79
minjoo <minjooshin@handong.ac.kr>
2024-07-05 16:35:54 +0900

    [PGS] Level2 다음 큰 숫자

---
+++ week14/다음 큰 숫자.cpp
@@ -0,0 +1,32 @@
+/*
+출처: 프로그래머스
+문제: 다음 큰 숫자
+난이도: Level2
+시간: 30분
+주소: https://school.programmers.co.kr/learn/courses/30/lessons/12911
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int getBinary(int num) {
+    int cnt = 0;
+    
+    while (num > 1) {
+        if (num % 2 == 1) cnt++;
+        num = num / 2;
+    }
+    return cnt++;
+}
+
+int solution(int n) {
+    int comp = n+1;
+    int n_cnt, comp_cnt, answer;
+    
+    while (comp > n) {
+        if (getBinary(n) == getBinary(comp)) break;
+        else comp++;
+    } 
+    return comp;
+}
